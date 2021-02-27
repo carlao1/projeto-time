297f4e3da44b6ec55334afb8afb919c857be0ea6
carlos roberto <carlossimeoneneto@gmail.com>
2021-02-27 10:55:52 -0300

    conexao ao banco criada

---
+++ ProjetoEscola.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoescola;
+
+import gui.Interface;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Carlos
+ */
+public class ProjetoEscola {
+
+   
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Interface().setVisible(true);
+            /*
+            try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (Exception e) {
+            }
+            
+            JFrame frame = new JFrame("JavaFX 2 in Swing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+            JApplet applet = new RoteiroOO();
+            applet.init();
+            
+            frame.setContentPane(applet.getContentPane());
+            
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            
+            applet.start();*/
+        });
+       // Professor p1 = new Professor();
+        
+        //p1.preencher();
+        //p1.imprimir();
+        
+        
+       //Professor p2 = new Professor();
+       
+      // p2.copiar(p1);
+      // p2.imprimir();
+       
+       
+     //  Aluno a1 = new Aluno();
+      // a1.preencher();
+      // a1.imprimir();
+       
+      // Aluno a2 = new Aluno();
+      // a2.copiar(a1);
+      // a2.imprimir();
+        
+//        Disciplina calculo = new Disciplina();
+//        calculo.preencher();
+//        calculo.imprimir();
+        
+        
+    }
+    
+}
